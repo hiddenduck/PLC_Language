abf20811c5b472b086d65532c1eb69175c09d31f
Lucenix <andrelucenaferreira@gmail.com>
2022-10-26 09:58:16 +0100

    brincadeiras com opens

--- tp1.py
+++ tp1.py
@@ -2,11 +2,14 @@ import re
 
 print("Split de uma linha por virgulas")
 
-input = open("teste1.csv", "r")
+input = open("teste1.csv", "r", encoding="utf-8")
 entradas = {}
 i = 0
 for linha in input:
     entradas[i] = re.split(r',', linha)
     i+=1
 input.close()
+output = open("output.txt", "w")
+for i in entradas:
+    output.write(str(entradas[i]) + "\n")
 print(entradas)
