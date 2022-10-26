7eaea63858ca7ee385103d0c968fc9248a8f71b1
Lucenix <andrelucenaferreira@gmail.com>
2022-10-26 20:31:47 +0100

    vai carlos corre!

--- tp1.py
+++ tp1.py
@@ -3,13 +3,14 @@ import re
 print("Split de uma linha por virgulas")
 
 input = open("emd.csv", "r", encoding="utf-8")
-entradas = {}
-entradas[0] = re.split(r'\,(?!s*[0-9]+})',linha[0:-1])
-for i,linha in enumerate(input[1:]):
+entradas = []
+entradas.append(re.split(r'\,(?!s*[0-9]+})', input.readline())) 
+
+for linha in input:
     linha = linha[:-1]
-     entradas[i+1] = re.split(r',', linha)
+    entradas.append(re.split(r',', linha))
 input.close()
 output = open("output.txt", "w")
-for i in entradas:
+for i in range(len(entradas)):
     output.write(str(entradas[i]) + "\n")
 print(entradas)
