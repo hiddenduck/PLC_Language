e10dd48fe9eddd9573870a72a07c96b88a9fa65f
Lucenix <andrelucenaferreira@gmail.com>
2022-10-26 22:30:23 +0100

    algum random work

--- tp1.py
+++ tp1.py
@@ -1,16 +1,25 @@
 import re
 
+colunas = []
+entrada = []
+
 print("Split de uma linha por virgulas")
 
 input = open("emd.csv", "r", encoding="utf-8")
-entradas = []
-entradas.append(re.split(r'\,(?!s*[0-9]+})', input.readline())) 
+colunas = re.split(r'\,(?!\s*[0-9]+})', input.readline()[:-1])
+print(colunas)
 
 for linha in input:
-    linha = linha[:-1]
-    entradas.append(re.split(r',', linha))
+    escrita = {}
+    r = 0
+    entrada = (re.split(r',', linha[:-1]))
+    for i in range(len(colunas)):
+        l = re.search(r'{\d+(\,\d+)?}', colunas[i])
+        if l:
+            print("listas")
+            #lidar com listas
+        else:
+            escrita[]
+
+    print(entrada)
 input.close()
-output = open("output.txt", "w")
-for i in range(len(entradas)):
-    output.write(str(entradas[i]) + "\n")
-print(entradas)
