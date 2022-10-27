db8934c85a011a27c4b3cb915be2efaa24251867
Lucenix <andrelucenaferreira@gmail.com>
2022-10-27 09:48:15 +0100

    dar uma de jbb e tentar por tudo no menor numero de linhas possivel

--- tp1.py
+++ tp1.py
@@ -5,34 +5,34 @@ colunas = []
 print("Split de uma linha por virgulas")
 
 input = open("teste1.csv", "r", encoding="utf-8")
-colunas = re.findall(r'(?<![^,])([^{^,]+)(?:{(\d+)(?:,(\d+))?})?(?:::([^,]+))?', input.readline()[:-1])
+primeira = input.readline()[:-1]
+colunas = re.findall(r'(?<![^,])([^{^,]+)(?:{(\d+)(?:,(\d+))?})?(?:::([^,]+))?', primeira)
 #será que é possível fazer sub(ou mesmo sub n) nesta string e construir logo algo super interessante, uma linha magica logo de estoura, evitando um for
 print(colunas)
 
 linha_magica = r''
+print(linha_magica)
 
 #colunas: 0 -> título; 1->min/único; 2->max; 3->função
 for coluna in colunas:
     if coluna[1] != '':
         #será possível escrever esta restrição sem obrigar que exista pelo menos um, para podermos usar 0
-        if coluna[2] != '':
+        if coluna[2] == '':
+            coluna[2] = coluna[1]
             #tentar evitar as vírgulas com espaços vazios porque torna o resto muito mais fácil
-            linha_magica += r'([^,]+(?:,[^,]+){%d,%d}),{1,%d}' % (int(coluna[1])-1, int(coluna[2])-1,int(coluna[2]) - int(coluna[1]) + 1)
-            #min e max
-        else:
-            linha_magica += r'([^,]*(?:,[^,]*){%d}),' % (int(coluna[1])-1)
-            #único
-        #no matching não preciso de fazer nada para a função
+        #apanha os casos todos logo, também só aumenta 5 bytes a cada string?
+        linha_magica += r'([^,]+(?:,[^,]+){%d,%d}),{1,%d}' % (int(coluna[1])-1, int(coluna[2])-1,int(coluna[2]) - int(coluna[1]) + 1)
     else:
         linha_magica += r'([^{,]+),'
 linha_magica = linha_magica[:-1]
 print(linha_magica)
 #o group de 0 é a linha sempre?
 for linha in input:
+    linha = linha[:-1]
     print(linha)
     teste = re.search(linha_magica, linha)
     for i in range(len(colunas)):
-        if (colunas[i][1] == ''):
+        if (colunas[i][1] != ''):
             lista = re.split(r',', teste.group(i+1))
             if (colunas[i][3] != ''):
                 fun = eval(colunas[i][3])
