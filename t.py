675bc9edb189541381382b9ee762304b69ef49e6
mirelois <79458822+mirelois@users.noreply.github.com>
2022-11-02 20:37:39 +0000

    era isto que estavas a pensar lucena?

---
+++ t.py
@@ -0,0 +1,51 @@
+import re
+
+
+
+
+
+
+
+f1 = eval("sum")
+#f2 = eval("mean")
+
+print(f1)
+#print(f2)
+
+l = []
+l.append(f1)
+#l.append(f2)
+
+i = 0
+
+def fun(i,line):
+    return str(l[i](map(int,re.split(r',',line))))
+
+
+line = '[1,2],[3,4]'
+
+line = re.sub(r'(1,2)',lambda x: fun(i,x.group(1)) ,line)
+
+
+print(line)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tp1 copy.py
+++ tp1 copy.py
@@ -4,70 +4,95 @@ colunas = []
 
 print("Split de uma linha por virgulas")
 
+fun_list = list()
+
 input = open("teste1.csv", "r", encoding="utf-8")
 primeira = input.readline()[:-1]
 colunas = re.findall(r'(?<![^,])([^{^,]+)(?:{(\d+)(?:,(\d+))?})?(?:::([^,]+))?', primeira)
+
+
 #será que é possível fazer sub(ou mesmo sub n) nesta string e construir logo algo super interessante, uma linha magica logo de estoura, evitando um for
 print(colunas)
 
-linha_magica = r''
-lm = lambda x: ''
-print(linha_magica)
+patern = r''
+replace = r''
+
+
+
+
+
+
+
+
 i = 1
+fun_pos = 0
 #colunas: 0 -> título; 1->min/único; 2->max; 3->função
 for coluna in colunas:
-    if coluna[1] != '':
-        #será possível escrever esta restrição sem obrigar que exista pelo menos um, para podermos usar 0
-        if coluna[2] == '':
-            coluna[2] = coluna[1]
-            #tentar evitar as vírgulas com espaços vazios porque torna o resto muito mais fácil
-        #apanha os casos todos logo, também só aumenta 5 bytes a cada string?
-        linha_magica += r'([^,]+(?:,[^,]+){%d,%d}),{0,%d},' % (int(coluna[1])-1, int(coluna[2])-1,int(coluna[2]) - int(coluna[1]))
+    if coluna[1] == '':
+        patern += r'([^{,]+),'    
+        replace += r'%s:\%d\n' % (coluna[0],i)
     else:
-        linha_magica += r'([^{,]+),'
-linha_magica = linha_magica[:-1]
-print(linha_magica)
-print(lm)
+        if coluna[2] == '':
+            if coluna[3] == '':
+                patern += r'([^,]+(?:,[^,]+){%d}),{0,%d},' % (int(coluna[1])-1, int(coluna[1])-1)
+                replace += r'%s:[\%d]\n' % (coluna[0],i)
+            else:
+                fun_list.append((eval(coluna[3]),fun_pos,coluna[1],''))
+                patern += r'([^{,]+),'    
+                replace += r'%s:\%d\n' % (coluna[0],i)
+                fun_pos += int(coluna[1])
+        else:
+            if coluna[3] == '':
+                patern +=   r'([^,]+(?:,[^,]+){%d,%d}),{0,%d},' % (int(coluna[1])-1, int(coluna[2])-1,int(coluna[2])-1) 
+                replace += r'%s:[\%d]\n' % (coluna[0],i)
+                fun_pos += int(coluna[2])
+            else:
+                fun_list.append((eval(coluna[3]),fun_pos,coluna[1],coluna[2]))
+                patern += r'([^{,]+),'    
+                replace += r'%s:\%d\n' % (coluna[0],i)
+    i += 1
+    fun_pos += 1
+
+
+
+patern = patern[:-1]
+print(patern)
+print(replace)
 #o group de 0 é a linha sempre?
+"""for linha in input:
+    linha = linha[:-1]
+    print(linha)
+    teste = re.search(linha_magica, linha)
+    for i in range(len(colunas)):
+        if (colunas[i][1] != ''):
+            lista = re.split(r',', teste.group(i+1))
+            if (colunas[i][3] != ''):
+                fun = eval(colunas[i][3])
+                print(colunas[i][0] + "_" + colunas[i][3] + " : " + str(fun(map(int,lista))))
+            else:
+                print(colunas[i][0] + " : " + str(lista))
+        else:
+            print(colunas[i][0] + " : " + teste.group(i+1))"""
+
+
 
 res = ''
+
+
+
+
 for linha in input:
     linha = linha[:-1]
-    for coluna in colunas:
-        if coluna[1] != '':
-            if coluna[2] != '':
-                if coluna[3] != '':
-                    fun = eval(coluna[3])
-                    linha = re.sub(r'([^,]+(?:,[^,]+){%d,%d})' % (int(coluna[1])-1, int(coluna[2])-1), r'%s_%s:\11111\n' % (coluna[0],coluna[3]), linha, count = 1)
-                    break    
-                linha = re.sub(r'([^,]+(?:,[^,]+){%d,%d})' % (int(coluna[1])-1, int(coluna[2])-1), (r'%s:[\1]2222222\n' % coluna[0]), linha, count = 1)
-                break
-        linha = re.sub(r'([^,]+(?:,[^,]+))', r'%s:\1333333333\n'%coluna[0], linha, count = 1)
-    res += linha
     print(linha)
-#print(res)
+    for fun,pos,lower,upper in fun_list:
+        if upper == '':
+            linha = re.sub(r'((?:[^,]+,){%d})([^,]+(?:,[^,]+){%d}),{0,%d}'%(pos,(int(lower)-1),int(lower)),lambda x: x.group(1) + str(fun(map(int,re.split(r',',x.group(2)))))+r',',linha)
+        else:
+            linha = re.sub(r'((?:[^,]+,){%d})([^,]+(?:,[^,]+){%d,%d}),{0,%d}'%(pos,(int(lower)-1),(int(upper)-1),int(upper)),lambda x: x.group(1) + str(fun(map(int,re.split(r',',x.group(2)))))+r',' ,linha)
+
+    res += re.sub(patern,replace,linha)
+    res += '\n'
+
+print(res)
 
 input.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-d = {
-    [3,2,1],
-    [3,2,1],
-    [3,2,1,4,5,6,7],
-    [3,2,1],
-    [1,2,3],
-    [1,2,3],
-}
-
