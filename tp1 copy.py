3a49ad34ff544fa32292250a8b99ccd61082b8c7
Lucenix <andrelucenaferreira@gmail.com>
2022-11-02 22:46:12 +0000

    mudanças meio manhosas c:

--- tp1 copy.py
+++ tp1 copy.py
@@ -1,5 +1,8 @@
 import re
 
+def teste1(lista):
+    return [[x] for x in lista]
+
 colunas = []
 
 print("Split de uma linha por virgulas")
@@ -14,15 +17,8 @@ colunas = re.findall(r'(?<![^,])([^{^,]+)(?:{(\d+)(?:,(\d+))?})?(?:::([^,]+))?',
 #será que é possível fazer sub(ou mesmo sub n) nesta string e construir logo algo super interessante, uma linha magica logo de estoura, evitando um for
 print(colunas)
 
-patern = r''
+patern = r'^'
 replace = r''
-
-
-
-
-
-
-
 
 i = 1
 fun_pos = 0
@@ -37,25 +33,23 @@ for coluna in colunas:
                 patern += r'([^,]+(?:,[^,]+){%d}),{0,%d},' % (int(coluna[1])-1, int(coluna[1])-1)
                 replace += r'%s:[\%d]\n' % (coluna[0],i)
             else:
-                fun_list.append((eval(coluna[3]),fun_pos,coluna[1],''))
-                patern += r'([^{,]+),'    
-                replace += r'%s:\%d\n' % (coluna[0],i)
+                fun_list.append((eval(coluna[3]),fun_pos,coluna[1],coluna[1]))
+                patern += r'((?:[^\[\],]+)|(?:\[[^,]*(?:,[^,]*)*\])),'    
+                replace += r'%s_%s:\%d\n' % (coluna[0],coluna[3],i)
                 fun_pos += int(coluna[1])
         else:
             if coluna[3] == '':
-                patern +=   r'([^,]+(?:,[^,]+){%d,%d}),{0,%d},' % (int(coluna[1])-1, int(coluna[2])-1,int(coluna[2])-1) 
+                patern += r'([^,]+(?:,[^,]+){%d,%d}),{0,%d},' % (int(coluna[1])-1, int(coluna[2])-1,int(coluna[2])-1) 
                 replace += r'%s:[\%d]\n' % (coluna[0],i)
                 fun_pos += int(coluna[2])
             else:
                 fun_list.append((eval(coluna[3]),fun_pos,coluna[1],coluna[2]))
-                patern += r'([^{,]+),'    
-                replace += r'%s:\%d\n' % (coluna[0],i)
+                patern += r'((?:[^\[\],]+)|(?:\[[^,]*(?:,[^,]*)*\])),'    
+                replace += r'%s_%s:\%d\n' % (coluna[0],coluna[3],i)
     i += 1
     fun_pos += 1
 
-
-
-patern = patern[:-1]
+patern = patern[:-1] + '$'
 print(patern)
 print(replace)
 #o group de 0 é a linha sempre?
@@ -74,22 +68,13 @@ print(replace)
         else:
             print(colunas[i][0] + " : " + teste.group(i+1))"""
 
-
-
 res = ''
-
-
-
-
 for linha in input:
     linha = linha[:-1]
     print(linha)
     for fun,pos,lower,upper in fun_list:
-        if upper == '':
-            linha = re.sub(r'((?:[^,]+,){%d})([^,]+(?:,[^,]+){%d}),{0,%d}'%(pos,(int(lower)-1),int(lower)),lambda x: x.group(1) + str(fun(map(int,re.split(r',',x.group(2)))))+r',',linha)
-        else:
-            linha = re.sub(r'((?:[^,]+,){%d})([^,]+(?:,[^,]+){%d,%d}),{0,%d}'%(pos,(int(lower)-1),(int(upper)-1),int(upper)),lambda x: x.group(1) + str(fun(map(int,re.split(r',',x.group(2)))))+r',' ,linha)
-
+        linha = re.sub(r'((?:[^,]+,){%d})([^,]+(?:,[^,]+){%d,%d}),{0,%d}'%(pos,(int(lower)-1),(int(upper)-1),int(upper)),lambda x: x.group(1) + str(fun(list(map(int,re.split(r',',x.group(2))))))+r',' ,linha)
+    print(linha)
     res += re.sub(patern,replace,linha)
     res += '\n'
 
