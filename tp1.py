20e2a7486d3eb9d6ba49a267bb3c1c42438535de
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2022-11-12 19:21:14 +0000

    PUSH

--- tp1.py
+++ tp1.py
@@ -7,7 +7,7 @@ def teste1(l):
 start = time.time()
 colunas = []
 
-input = open("emd.csv", "r", encoding="utf-8")
+input = open("teste1.csv", "r", encoding="utf-8")
 primeira = input.readline()[:-1]
 colunas = re.findall(r'(?<![^,])([^{^,]+)(?:{(\d+)(?:,(\d+))?})?(?:::([^,]+))?', primeira)
 #será que é possível fazer sub(ou mesmo sub n) nesta string e construir logo algo super interessante, uma linha magica logo de estoura, evitando um for
@@ -15,18 +15,16 @@ colunas = re.findall(r'(?<![^,])([^{^,]+)(?:{(\d+)(?:,(\d+))?})?(?:::([^,]+))?',
 res = '[\n'
 #0->título, 1->min, 2->max, 3->fun
 for linha in input:
-    linha = linha[:-1]
+    linha = re.sub(r'\n$', r'', linha)
     split = re.split(r',', linha)
-    r = 0
+    r = 0 #ponto da lista split a ler
     res += '\t{\n'
     for i in range(len(colunas)):
         if colunas[i][1] == '':
             res += '\t\t\"%s\": \"%s\",\n' % (colunas[i][0], split[r])
             r += 1
         else:
-            max = int(colunas[i][1])
-            if colunas[i][2] != '':
-                max = int(colunas[i][2])
+            max = int(colunas[i][1]) if colunas[i][2] == "" else int(colunas[i][2])
             lis = []
             for l in range(max):
                 if split[r] != '':
@@ -37,8 +35,8 @@ for linha in input:
             else:
                 fun = eval(colunas[i][3])
                 res+='\t\t\"%s_%s\": %s,\n' % (colunas[i][0], colunas[i][3], str(fun(map(int,lis))))
-    res = res[:-2] + '\n\t},\n'
-res = res[:-2] + '\n]'
+    res = re.sub(r',\n$', '\n\t},\n', res) #res[:-2] + '\n\t},\n'
+res = re.sub(r',\n$', '\n]', res) #res[:-2] + '\n]'
 print(res)
 
 end = time.time()
