7472a44fedf34eb1d92e6e5baaaae8a8041a4941
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2022-12-28 15:15:53 +0000

    Mais codigo

--- TP2/yacc.py
+++ TP2/yacc.py
@@ -7,50 +7,52 @@ from lex import tokens
 
 def p_axiom_code(p):
     "Axiom : Axiom Code"
+    p[0] = p[1] + p[2]
 
 
 def p_axiom_empty(p):
     "Axiom : "
-
+    p[0] = ""
 
 def p_code_block(p):
     "Code : Code Block"
-
+    p[0] = p[1] + p[2]
 
 def p_code_empty(p):
     "Code : "
-
+    p[0] = ""
 
 def p_block_exp(p):
     "Block : Exp ';'"
-
+    p[0] = p[1]
 
 def p_block_if(p):
     "Block : If "
-
+    p[0] = p[1]
 
 def p_block_ifelse(p):
     "Block : IfElse "
-
+    p[0] = p[1]
 
 def p_block_while(p):
     "Block : While "
-
+    p[0] = p[1]
 
 def p_block_switch(p):
     "Block : Switch "
-
+    p[0] = p[1]
 
 def p_block_empty(p):
     "Block : "
-
+    p[0] = ""
 
 def p_body_block(p):
     "Body : Block"
-
+    p[0] = p[1]
 
 def p_body_code(p):
     "Body : '{' Code '}'"
+    p[0] = p[2]
 
 def p_if_scope(p):
     "IfScope : IF"
@@ -107,14 +109,17 @@ def p_case(p):
 
 def p_exp_atrib(p):
     "Exp: Atrib"
+    p[0] = p[1]
 
 
 def p_exp_op(p):
     "Exp: Op"
+    p[0] = p[1]
 
 
 def p_exp_decl(p):
-    "Exp: Decl" 
+    "Exp: Decl"
+    p[0] = p[1]
 
 def p_exp_declatrib(p):
     "Exp: DeclAtrib"
@@ -161,29 +166,64 @@ def p_decl(p):
     p.parser.n_local_vars += 1
 
 
+def gen_atrib_code(p, id, exp):
+    s = ""
+    if id not in p.parser.id_table:
+        print("ERROR: Name %s not defined." % id)
+        #invoke error
+    elif p.parser.id_table[id]['scope'] == 0:
+        #pensar que se deve colocar no topo da stack o valor no ID para se conseguir fazer a operação
+        s = exp + "storeg %d\n" % p.parser.id_table[id]['endereco']
+    elif p.parser.id_table[id]['scope'] == p.parser.scope:
+        s = exp + "storel %d\n" % p.parser.id_table[id]['endereco']
+    else:
+        print("ERROR: Name %s defined elsewhere in program, not defined in local scope." % id)
+        #invoke error
+    return s
+
 def p_atrib_left(p):
     "Atrib: ID LARROW Exp"
-
+    p[0] = gen_atrib_code(p, p[1], p[3])
 
 def p_atrib_right(p):
     "Atrib: Exp RARROW ID"
-
+    p[0] = gen_atrib_code(p, p[3], p[1])
 
 def p_atrib_equiv(p):
     "Atrib: ID SWAP ID"
-
+    if p[1] not in p.parser.id_table:
+        print("ERROR: Name %s not defined." % p[1])
+        #invoke error
+    if p[3] not in p.parser.id_table:
+        print("ERROR: Name %s not defined." % p[3])
+        #invoke error
+    s1 = p.parser.id_table[p[1]]['scope']
+    s3 = p.parser.id_table[p[3]]['scope']
+    if s1 == 0: s1 = "g"
+    elif s1 == p.parser.scope: s1 = "l"
+    else:
+        print("ERROR: Name %s defined elsewhere in program, not defined in local scope." % p[1])
+        #invoke error
+    if s3 == 0: s3 = "g"
+    elif s3 == p.parser.scope: s3 = "l"
+    else:
+        print("ERROR: Name %s defined elsewhere in program, not defined in local scope." % p[3])
+        #invoke error
+    p[0] = "push" + s3 + p.parser.id_table[p[3]]['endereco'] + "\npush" + s1 + p.parser.id_table[p[1]]['endereco'] 
+    + "\nstore" + s1 + p.parser.id_table[p[1]]['endereco'] + "\nstore" + s3 + p.parser.id_table[p[3]]['endereco'] + "\n"
 
 def p_op_opuno(p):
     "Op: OpUno"
+    p[0] = p[1]
 
 
 def p_op_opbin(p):
     "Op: OpBin"
-
+    p[0] = p[1]
 
 def p_opuno_neg(p):
     "OpUno: NEG Exp"
-
+    p[0] = p[2] + 'not\n'
 
 def p_opuno_index(p):
     "OpUno: ID '[' Exp ']'"
@@ -191,104 +231,122 @@ def p_opuno_index(p):
 
 def p_opbin_rec(p):
     "OpBin: OpBin OpLogic TermPlus"
-
+    p[0] = p[1] + p[3] + p[2]
 
 def p_opbin_base(p):
     "OpBin: TermPlus"
-
+    p[0] = p[1]
 
 def p_termplus_rec(p):
     "TermPlus: TermPlus OpPlus TermMult"
-
+    p[0] = p[1] + p[3] + p[2]
 
 def p_termplus_base(p):
     "TermPlus: TermMult"
-
+    p[0] = p[1]
 
 def p_termmult_rec(p):
     "TermMult: TermMult OpMult TermPow"
-
+    p[0] = p[1] + p[3] + p[2]
 
 def p_termmult_base(p):
     "TermMult: TermPow"
-
+    p[0] = p[1]
 
 def p_termpow_rec(p):
     "TermPow: TermPow OpPow Base"
-
+    p[0] = p[1] + p[3] + p[2]
 
 def p_termmult_base(p):
     "TermPow: Base"
-    
+    p[0] = p[1]
 
 def p_base_exp(p):
     "Base: '(' Exp ')'"
-    p[0] =   p[2]
+    p[0] = p[2]
 
 
 def p_base_id(p):
     "Base: ID"
-    p[0] = p[1]
+    if p[1] not in p.parser.id_table:
+        print("ERROR: Name %s not defined." % p[1])
+        #invoke error
+    elif p.parser.id_table[p[1]]['scope'] == 0:
+        #pensar que se deve colocar no topo da stack o valor no ID para se conseguir fazer a operação
+        p[0] = "pushg %d\n" % p.parser.id_table[p[1]]['endereco']
+    elif p.parser.id_table[p[1]]['scope'] == p.parser.scope:
+        p[0] = exp + "pushl %d\n" % p.parser.id_table[p[1]]['endereco']
+    else:
+        print("ERROR: Name %s defined elsewhere in program, not defined in local scope." % p[1])
+        #invoke error
 
+
+    if p[1] not in p.parser.id_table:
+        print("Name %s not defined." % p[1])
+    if p[1]['scope'] == 0:
+    #pensar que se deve colocar no topo da stack o valor no ID para se conseguir fazer a operação
+        p[0] = "pushl %d\n" % p[1]['endereco']
+    elif p[1]['scope'] != p.parser.scope:
+        print("Name %s defined elsewhere in program, not defined in local scope." % p[1])
 
 def p_base_num(p):
     "Base: NUM"
-    p[0] = p[1]
+    p[0] = "pushi %d\n" % p[1]
 
 
 def p_oplogico_and(p):
     "OpLogico: AND"    
-    p[0] = p[1]
+    p[0] = "and\n"
 
 
 def p_oplogico_or(p):
     "OpLogico: OR"    
-    p[0] = p[1]
+    p[0] = "or\n"
 
 
 def p_oplogico_less(p):
     "OpLogico: LESS"    
-    p[0] = p[1]
+    p[0] = "inf\n"
 
 
 def p_oplogico_greater(p):
     "OpLogico: GREATER"    
-    p[0] = p[1]
+    p[0] = "sup\n"
 
 
 def p_oplogico_leq(p):
     "OpLogico: LEQ"   
-    p[0] = p[1]
+    p[0] = "infeq\n"
 
 
 def p_oplogico_geq(p):
     "OpLogico: GEQ"  
-    p[0] = p[1]
+    p[0] = "supeq\n"
 
 
 def p_oplogico_equal(p):
     "OpLogico: EQUAL"
-    p[0] = p[1]
+    p[0] = "equal\n"
 
 
 def p_opplus_add(p):
     "OpPlus: ADD"
-    p[0] = p[1]
+    p[0] = "add\n"
 
 
 def p_opplus_sub(p):
     "OpPlus: SUB"
-    p[0] = p[1]
+    p[0] = "sub\n"
 
 
 def p_opmult_mul(p):
     "OpMult: MUL"
-    p[0] = p[1]
+    p[0] = "mul\n"
 
 
 def p_opmult_div(p):
     "OpMult: DIV"
-    p[0] = p[1]
+    p[0] = "div\n"
 
 
 def p_oppow(p):
